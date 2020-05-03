db919a9e79e3b6582c0ca92df0284dc144b3267e
tdbnz-coding <>
2020-05-03 14:17:42 +1200

    Scam Baiting
    *Fix Scam Baiting addon
    
    *remvoe kodi youtubers to out of date

--- plugin.video.scamabc/ngb.py
+++ plugin.video.scamabc/ngb.py
@@ -18,30 +18,34 @@ import plugintools
 import xbmc,xbmcaddon
 from addon.common.addon import Addon
 
-addonID = 'plugin.video.scambaiters'
+addonID = 'plugin.video.kodiyoutubers'
 addon = Addon(addonID, sys.argv)
 local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
 xbmc.executebuiltin('Container.SetViewMode(500)')
 
-YOUTUBE_CHANNEL_ID_1 = "UCBNG0osIBAprVcZZ3ic84vw" #Jim Browning
-YOUTUBE_CHANNEL_ID_2 = "UCC9EjyMN_hx5NdctLBx5X7w" #Scammer Payback
-YOUTUBE_CHANNEL_ID_3 = "UCUZb_t6bIRSwyUeEaU6e5xQ" #Scammer Payback Live
-YOUTUBE_CHANNEL_ID_4 = "UC0uJKUXiU5T41Fzawy5H6mw" #ScammerRevolts
-YOUTUBE_CHANNEL_ID_5 = "UCm22FAXZMw1BaWeFszZxUKw" #Kitboga
-YOUTUBE_CHANNEL_ID_6 = "UC_0osV_nf2b0sIbm4Wiw4RQ" #IRLrosie 
-YOUTUBE_CHANNEL_ID_7 = "UCxzXRl9vxhPhyYITOlPW9dg" #Deeveeaar
-YOUTUBE_CHANNEL_ID_8 = "UC_b1bYSup-dHfwIoIXP0zPQ" #Malcolm Merlyn
-YOUTUBE_CHANNEL_ID_9 = "UC7AyZF0m_WV4GSXcBOg8V5Q" #BobHasAVirus
-YOUTUBE_CHANNEL_ID_10 = "UCxCJ93k6uk-qarMgFpX-Otw"#ScamBait Central
-YOUTUBE_CHANNEL_ID_11 = "UCqTQnJ_Wk0PAzWCOHMfefDQ"#NeeP
-YOUTUBE_CHANNEL_ID_12 = "UCx9yZ74cCszjui8H90cUN8A"#Thunder Dragon
-YOUTUBE_CHANNEL_ID_13 = "UCnNlJNSRxa3PF8XrKHOEPug"#Hoax Hotel
-YOUTUBE_CHANNEL_ID_14 = "UCLZxPVkEvf9XuifStuHK_gA"#Lewis's Tech
-YOUTUBE_CHANNEL_ID_15 = "UCdFypR5fbIFjaA5csQpgVQg"#IRS I reck scammers
-YOUTUBE_CHANNEL_ID_16 = "UCAf6Q_sHACA__65xc37waLQ"#IT Advocate
-YOUTUBE_CHANNEL_ID_17 = "UCTKTCy5UBdCqnGcj2f4Ozug"#JayBeeTV
+YOUTUBE_CHANNEL_ID_1 = "UCBNG0osIBAprVcZZ3ic84vw" 
+YOUTUBE_CHANNEL_ID_2 = "UCC9EjyMN_hx5NdctLBx5X7w" 
+YOUTUBE_CHANNEL_ID_3 = "UCUZb_t6bIRSwyUeEaU6e5xQ" 
+YOUTUBE_CHANNEL_ID_4 = "UC0uJKUXiU5T41Fzawy5H6mw" 
+YOUTUBE_CHANNEL_ID_5 = "UCm22FAXZMw1BaWeFszZxUKw" 
+YOUTUBE_CHANNEL_ID_6 = "UC_0osV_nf2b0sIbm4Wiw4RQ"  
+YOUTUBE_CHANNEL_ID_7 = "UCxzXRl9vxhPhyYITOlPW9dg" 
+YOUTUBE_CHANNEL_ID_8 = "UC_b1bYSup-dHfwIoIXP0zPQ" 
+YOUTUBE_CHANNEL_ID_9 = "UC7AyZF0m_WV4GSXcBOg8V5Q" 
+YOUTUBE_CHANNEL_ID_10 = "UCxCJ93k6uk-qarMgFpX-Otw"
+YOUTUBE_CHANNEL_ID_11 = "UCqTQnJ_Wk0PAzWCOHMfefDQ"
+YOUTUBE_CHANNEL_ID_12 = "UCx9yZ74cCszjui8H90cUN8A"
+YOUTUBE_CHANNEL_ID_13 = "UCnNlJNSRxa3PF8XrKHOEPug"
+YOUTUBE_CHANNEL_ID_14 = "UCLZxPVkEvf9XuifStuHK_gA"
+YOUTUBE_CHANNEL_ID_15 = "UCdFypR5fbIFjaA5csQpgVQg"
+YOUTUBE_CHANNEL_ID_16 = "UCAf6Q_sHACA__65xc37waLQ"
+YOUTUBE_CHANNEL_ID_17 = "UCTKTCy5UBdCqnGcj2f4Ozug"
+
+
+
+
 
 
 
@@ -107,27 +111,27 @@ def main_list(params):
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_6+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJz9ZU477gdFP6jcaVLA8Fc7N-rCjFwe1RKAkg=s176-c-k-c0x00ffffff-no-rj-mo",
         folder=True )
-		
+        
     plugintools.add_item( 
         #action="", 
         title="Deeveeaar",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_7+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJypJVfVInV0OeM4l3LD85m17Taax2c7FFLfsw=s176-c-k-c0x00ffffff-no-rj-mo",
         folder=True )
-		
+        
     plugintools.add_item( 
         #action="", 
         title="Malcolm Merlyn",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_8+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJxEzKN2LjOHp5z96PRL3T0Th63FkQuXSO-1Qg=s176-c-k-c0x00ffffff-no-rj-mo",
-        folder=True )	
-		
+        folder=True )   
+        
     plugintools.add_item( 
         #action="", 
         title="BobHasAVirus",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_9+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJzWZc81BXjWLKCPFXW0iZ5kUzh7OUmLOgrTUA=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
+        folder=True )   
 
 
     plugintools.add_item( 
@@ -135,57 +139,55 @@ def main_list(params):
         title="ScamBait Central",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_10+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJwubiCD9tLrJx80zGERFl7M7RItq_XN3_hKgA=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-	
-	plugintools.add_item( 
+        folder=True )   
+    
+    plugintools.add_item( 
         #action="", 
         title="NeeP",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_11+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJyYS7IMctlZw5NXnUwJ3VIePadfdlXk4wdIYw=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-	
+      folder=True )   
+    
 
     plugintools.add_item( 
         #action="", 
         title="Thunder Dragon",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_12+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJzuB7NyKyFby-PU9pl7zc5h5HWtAONxKAq5Uw=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-	
-	plugintools.add_item( 
+        folder=True )   
+    
+    plugintools.add_item( 
         #action="", 
         title="The Hoax Hotel",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_13+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJwHCzAQo_u3MTQgTnu-BZAsCDeIpN35SS9rKQ=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-	
-	plugintools.add_item( 
+        folder=True )   
+    
+    plugintools.add_item( 
         #action="", 
         title="Lewis's Tech",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_14+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJzQ0KXvcMhXxbSlD9lOciIeoJMuuELNKkszOQ=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
+        folder=True )   
 
-	plugintools.add_item( 
+    plugintools.add_item( 
         #action="", 
         title="IRS I reck scammers",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_15+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJxvwEEJpOkJBMAB-JD0O7GcitjiL9BnTfgFaw=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-	
-	plugintools.add_item( 
+        folder=True )   
+    
+    plugintools.add_item( 
         #action="", 
         title="IT Advocate",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_16+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJyHlZmfKSEkRATGSztOOSfOePum7wMjs7B3qA=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )		
+        folder=True )       
 
-	plugintools.add_item( 
+    plugintools.add_item( 
         #action="", 
         title="JayBeeTV",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_17+"/",
         thumbnail="https://yt3.ggpht.com/a/AATXAJxM71HTvvVqoQ2awSPeV-VOg6We5_s-N-VAaQ=s176-c-k-c0x00ffffff-no-rj-mo",
-		folder=True )	
-		
-  						
+        folder=True )   					
 run()
