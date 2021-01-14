c4cd081e92ac47a5a2c8b677022777973be8a016
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2021-01-15 09:13:16 +1300

    update repo

--- plugin.program.iptv.merge/resources/lib/constants.py
+++
@@ -1,9 +0,0 @@
-PLAYLIST_FILE_NAME  = 'playlist.m3u8'
-EPG_FILE_NAME       = 'epg.xml'
-
-IPTV_SIMPLE_ID      = 'pvr.freeviewnzandroid'
-INTEGRATIONS_URL    = 'https://k.slyguy.xyz/.iptv_merge/data.json.gz'
-
-METHOD_PLAYLIST     = 'playlist'
-METHOD_EPG          = 'epg'
-MERGE_SETTING_FILE  = '.iptv_merge'
